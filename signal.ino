7ee23a758c708178bb43dfec21556f6774464450
TheBushBoy <theodavid@icloud.com>
2023-02-16 17:59:31 +0100

    Change the states only when the waveform is finished

--- signal.ino
+++ signal.ino
@@ -1,38 +1,49 @@
-// Global variables
-int a = 400;
-int b = 100;
-int c = 9;
-int d = 4500;
-int countPulses = 0;
-
 // Pin definitions
-const short pinLedA = 15;
-const short pinLedB = 21;
-const short pinBtn1 = 22;
-const short pinBtn2 = 23;
+const unsigned short pinLedA = 15;
+const unsigned short pinLedB = 21;
+const unsigned short pinBtn1 = 22;
+const unsigned short pinBtn2 = 23;
+
+// Global variables
+const unsigned short a = 400;
+const unsigned short b = 100;
+const unsigned short c = 9;
+const unsigned short d = 4500;
+
+// Count number of pulses in one waveform
+unsigned char countPulses = 0;
 
 // Switch selected
-short state1 = 1;
-short state2 = 1;
+bool state1 = 1;
+bool state2 = 1;
 
 // Button state
-int btn1 = LOW;
-int btn2 = LOW;
-int oldBtn1 = 0;
-int oldBtn2 = 0;
+unsigned short btn1 = LOW;
+unsigned short btn2 = LOW;
+bool oldBtn1 = 0;
+bool oldBtn2 = 0;
+
+// Remember that a button have been pressed to change the state
+// only at the end of the waveform
+bool pressed1 = 0;
+bool pressed2 = 0;
 
 // Store previous counters of time
-int currentMicros;
-int previousMicros = 0;
+unsigned int currentMicros;
+unsigned int previousMicros = 0;
 
+// A value to check if the waveform is playing now
+bool isPlaying = 0;
+
+// Pin configuration
 void setup() {
-  // Pin configuration
   pinMode(pinLedA, OUTPUT);
   pinMode(pinLedB, OUTPUT);
   pinMode(pinBtn1, INPUT_PULLUP);
   pinMode(pinBtn2, INPUT_PULLUP);
 }
 
+// Check if the buttons have been pressed and change the states
 void updateBtnState() {
   // Read state of the buttons
   btn1 = digitalRead(pinBtn1);
@@ -41,13 +52,15 @@ void updateBtnState() {
   delay(10);
   
   // When button 1 is pressed, we change state beetween 0 and 1
-  if(btn1 && !oldBtn1) {
-    state1 = (state1 + 1) % 2; 
+  if(btn1 && !oldBtn1) { 
     oldBtn1 = 1;
-    
-    // By pressing the button it reset the signal
-    countPulses = 0;
-    previousMicros = 0;
+    pressed1 = 1;
+  }
+  // If the waveform is finished, we change the state
+  // if the button have been pressed
+  if(!isPlaying && pressed1) {
+    state1 = (state1 + 1) % 2;
+    pressed1 = 0;
   }
   else if(!btn1 && oldBtn1) {
     oldBtn1 = 0;
@@ -55,18 +68,21 @@ void updateBtnState() {
 
   // When button 2 is pressed, we change state beetween 0 and 1
   if(btn2 && !oldBtn2) {
-    state2 = (state2 + 1) % 2; 
     oldBtn2 = 1;
-    
-    // By pressing the button it reset the signal
-    countPulses = 0;
-    previousMicros = 0;
+    pressed2 = 1;
+  }
+  // If the waveform is finished, we change the state
+  // if the button have been pressed
+  if(!isPlaying && pressed2) {
+    state2 = (state2 + 1) % 2;
+    pressed2 = 0;
   }
   else if(!btn2 && oldBtn2) {
     oldBtn2 = 0;
   }
 }
 
+// Function that emmit the signal
 void signal(int nbOfPulses = c) {
   // We use a value to count the duration of the pulses
   currentMicros = micros();
@@ -75,19 +91,22 @@ void signal(int nbOfPulses = c) {
   if(currentMicros - previousMicros < 50 * 1000 && countPulses == 0) {
     digitalWrite(pinLedB, HIGH);
     digitalWrite(pinLedA, LOW);
+    isPlaying = true;
   }
 
   // Signal A
-  // State 1
+  // 1
   else if(currentMicros - previousMicros < 50 + (a + 50 * countPulses) * 1000) {
     digitalWrite(pinLedB, LOW);
     digitalWrite(pinLedA, HIGH);
+    isPlaying = true;
   }
 
-  // State 0
+  // 0
   else if(currentMicros - previousMicros < 50 + (a + 50 * countPulses + b) * 1000) {
     digitalWrite(pinLedB, LOW);
     digitalWrite(pinLedA, LOW);
+    isPlaying = true;
   }
 
   // After c pulses we reset countPulses and we wait for d us
@@ -95,10 +114,12 @@ void signal(int nbOfPulses = c) {
     if(currentMicros - previousMicros < d * 1000) {
       digitalWrite(pinLedB, LOW);
       digitalWrite(pinLedA, LOW);
+      isPlaying = true;
     }
     else {
       countPulses = 0;
       previousMicros = currentMicros;
+      isPlaying = false;
     }
   }
 
@@ -113,12 +134,12 @@ void loop() {
   // Check state of the buttons
   updateBtnState();
 
-  // When we press the button, the sequence is played
+  // When we press the button 1, the sequence is played
   if(state1) {
     // Reset Timer and enable stream of pulses,
     // if switch 2 is enable, we are in mode 1 with 3 pulses, 9 pulses otherwise
     if(state2) {
-      signal(3);
+      signal(6);
     }
     else {
       signal();
